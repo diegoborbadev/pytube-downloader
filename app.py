df13d613353d38e18b09436141324acb1f2faecd
Diego Borba <diegoborba25.contato@gmail.com>
2023-11-30 18:02:10 -0300

    Start of stream selection implementations

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from downloader import download
-from cleaner import clear_directory
+from time import time
+from pytube import YouTube
 from threading import Thread
+from cleaner import clear_directory
 from flask import Flask, render_template, request, send_file
 
 # App instance
@@ -16,23 +17,45 @@ def index():
     return render_template('index.html')
 
 # Download endpoint
-@app.route('/', methods=['POST'])
-def download():
-    #TODO: VERIFY DOWNLOAD TYPE PROPPERLY
-    type = request.form['type']
-
+@app.route('/streams', methods=['POST'])
+def streams():
     # Media link
     link = request.form['text']
     
+    # Download type:
+    type = request.form['type']
+
+    # Instance pytube
+    pytube = YouTube(link)
+
+    # Get streams
+    is_audio = type == 'audio'
+    if(is_audio):
+        streams = pytube.streams.filter(type=type, progressive="False").order_by("abr").desc()
+    else:
+        streams = pytube.streams.filter(type=type, progressive="False").order_by("resolution").desc()
+
+    return render_template('streams.html', link=link, streams=streams, is_audio=is_audio)
+    
+@app.route('/download', methods=['POST'])
+def download(link : str, audio: bool):
+    # Instance pytube
+    pytube = YouTube(link)
+
+    # Get the stream
+    stream = pytube.streams.get_audio_only() if audio else pytube.streams.get_highest_resolution()
+
+    # Generate filename
+    filename = f'temp{round(time() * 1000)}.{"mp3" if audio else "mp4"}'
+
+    # Download
     try:
-        # Get the file (locally)
-        filename = download(link, type == 'audio')
-        
+        stream.download(output_path='temp', filename=filename)
         # Download the file (browser)
         return send_file( f'temp/{filename}', as_attachment=True)
     except:
         # Render index again (with error)
-        return render_template('index.html', error='Invalid link')
+        return render_template('index.html', error='Invalid link')   
 
 # Scope verification
 if __name__ == "__main__":
